09a6dc2cfcf7ffb5b6d407749ba10b9337ba7cd5
abudluan <abudluan@gmail.com>
2023-11-22 14:06:26 -0300

    chore: handleFileUpload 1 e 2 adicionados

--- src/components/Form1/Form1.js
+++ src/components/Form1/Form1.js
@@ -9,7 +9,8 @@ const Form1 = () => {
     const [formData, setFormData] = useState({
         "entry.1830245759": "",
         "entry.753111962": "",
-        "entry.2122914383": "sem documento"
+        "entry.2122914383": "sem documento",
+        "entry.1709330095": "sem comprovante"
     });
 
     const handleInputData = (input) => (e) => {
@@ -25,25 +26,36 @@ const Form1 = () => {
         e.preventDefault();
         setSubmit(true);
 
-        let url = `https://docs.google.com/forms/d/e/1FAIpQLSffrX55aeqhkSSAKDr7o6AvkUsBDTG2a3S2Wmy3LoRztUpHgg/formResponse?entry.1830245759=${formData["entry.1830245759"]}&entry.753111962=${formData["entry.753111962"]}&entry.2122914383=${formData["entry.2122914383"]}`;
+        try {
+            let url = `https://docs.google.com/forms/d/e/1FAIpQLSffrX55aeqhkSSAKDr7o6AvkUsBDTG2a3S2Wmy3LoRztUpHgg/formResponse?entry.1830245759=${formData["entry.1830245759"]}&entry.753111962=${formData["entry.753111962"]}&entry.2122914383=${formData["entry.2122914383"]}&entry.1709330095=${formData["entry.1709330095"]}`;
 
-        const res = await fetch(url, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded"
+            const res = await fetch(url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded"
+                }
+            });
+
+            if (!res.ok) {
+                console.error("Falha ao enviar o formulário. Status do erro:", res.status);
+
             }
-        });
-        // Reseta o estado de loading e submit após o envio do formulário
-        setLoading(false);
-        setSubmit(false);
+
+        } catch (error) {
+            console.error("Erro ao fazer a solicitação:", error);
+
+        } finally {
+            setLoading(false);
+        }
     }
 
-    const fileInputRef = useRef(null);
+    const fileInputRef1 = useRef(null);
+    const fileInputRef2 = useRef(null);
 
-    const handleFileUpload = async () => {
+    const handleFileUpload1 = async () => {
         setLoading(true);
 
-        const files = fileInputRef.current.files;
+        const files = fileInputRef1.current.files;
         if (files.length > 0) {
             const formData1 = new FormData();
             for (let file of files) {
@@ -69,21 +81,65 @@ const Form1 = () => {
             } catch (error) {
                 console.error("Erro ao enviar arquivo:", error);
             } finally {
-                setLoading(false); // Desativa o estado de carregamento
+                setLoading(false);
             }
         }
     };
 
+    const handleFileUpload2 = async () => {
+        setLoading(true);
+
+        const files = fileInputRef2.current.files;
+        if (files.length > 0) {
+            const formData1 = new FormData();
+            for (let file of files) {
+                formData1.append('files', file);
+            }
+
+            try {
+                const response = await fetch('https://fullstackers.com.br:7443/upload', {
+                    method: 'POST',
+                    body: formData1
+                });
+
+                if (response.ok) {
+                    const data = await response.json();
+                    setFormData((prevState) => ({
+                        ...prevState,
+                        "entry.1709330095": data.files[0].id
+                    }));
+                    console.log(`id = ${data.files[0].id}`);
+                } else {
+                    console.error("Falha ao fazer upload do arquivo");
+                }
+            } catch (error) {
+                console.error("Erro ao enviar arquivo:", error);
+            } finally {
+                setLoading(false);
+            }
+        }
+    };
+
+    const handleNewFormClick = () => {
+        window.location.reload();
+    }
+
     return (
         <Container>
-            <Card className="mt-4">
-                <Card.Header>
-                    <h3 className="mt-2"><strong>Solicitação de Reconhecimento de Prescrição Administrativa de Débito Fiscal Inscrito em Dívida Ativa</strong></h3>
-                    <p className="mt-3">O interessado abaixo identificado, solicita o reconhecimento da prescrição para a extinção do crédito fiscal, com amparo no <strong>Art. 53-B da Lei nº 6.182/1998 c/c Instrução Normativa - IN SEFA nº 18/2020</strong>.</p>
-                </Card.Header>
+            {submit ? (
+                <Card className="mt-4 pb-3 pt-3">
+                    <h2 className="text-center">Formulário enviado com sucesso!</h2>
+                    <Button className="mx-auto mt-4 btn-success" onClick={handleNewFormClick}>Enviar novo formulário</Button>
+                </Card>
+            ) : (
+                <Card className="mt-4">
+                    <Card.Header>
+                        <h3 className="mt-2"><strong>Solicitação de Reconhecimento de Prescrição Administrativa de Débito Fiscal Inscrito em Dívida Ativa</strong></h3>
+                        <p className="mt-3">O interessado abaixo identificado, solicita o reconhecimento da prescrição para a extinção do crédito fiscal, com amparo no <strong>Art. 53-B da Lei nº 6.182/1998 c/c Instrução Normativa - IN SEFA nº 18/2020</strong>.</p>
+                    </Card.Header>
 
-                <Card.Body>
-                   
+                    <Card.Body>
+
                         <Form onSubmit={handleSubmit} target="_self">
 
                             <Form.Group className="mb-3">
@@ -133,34 +189,63 @@ const Form1 = () => {
                                 <Form.Control
                                     className="form-control"
                                     type="file"
-                                    ref={fileInputRef}
+                                    ref={fileInputRef1}
                                     id="documento"
                                     accept=".pdf"
-                                    onChange={handleFileUpload}
+                                    onChange={handleFileUpload1}
+                                    required
                                 />
-                                
+
                             </Form.Group>
 
                             <Form.Group className="mb-3">
-                                <Form.Label htmlFor="entry.2122914383" className="form-label">id documento</Form.Label>
                                 <Form.Control
                                     type="text"
                                     className="form-control"
                                     name="entry.2122914383"
                                     value={formData["entry.2122914383"]}
+                                    readOnly
                                 />
                             </Form.Group>
 
+                            <Form.Group className="mb-3">
+                                <Form.Label className="form-label">Comprovante de residência</Form.Label>
+                                <Form.Control
+                                    className="form-control"
+                                    type="file"
+                                    ref={fileInputRef2}
+                                    id="documento"
+                                    accept=".pdf"
+                                    onChange={handleFileUpload2}
+                                    required
+                                />
+
+                            </Form.Group>
+
+                            <Form.Group className="mb-3">
+                                <Form.Control
+                                    type="text"
+                                    className="form-control"
+                                    name="entry.1709330095"
+                                    value={formData["entry.1709330095"]}
+                                    readOnly
+                                />
+                            </Form.Group>
+
+
+
                             {loading && <div>Carregando...</div>}
 
-                            <Button type="submit" disabled={loading}>
+                            <Button className="btn-success " type="submit" disabled={loading}>
                                 Enviar
                             </Button>
 
                         </Form>
-                    
-                </Card.Body>
-            </Card >
+
+                    </Card.Body>
+
+                </Card >
+            )}
         </Container >
     );
 }
