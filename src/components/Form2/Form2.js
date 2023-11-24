227c71e694a84f8af4c252e3330cd6c0af152f05
abudluan <abudluan@gmail.com>
2023-11-24 11:00:07 -0300

    fix: accordion form2

--- src/components/Form2/Form2.js
+++ src/components/Form2/Form2.js
@@ -147,13 +147,40 @@ const Form1 = () => {
                             <Accordion.Item eventKey="0">
                                 <Accordion.Header><strong>Leia com atenção:</strong></Accordion.Header>
                                 <Accordion.Body>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                                    aliquip ex ea commodo consequat. Duis aute irure dolor in
-                                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                                    culpa qui officia deserunt mollit anim id est laborum.
+                                    <strong>1</strong> - De acordo com o Termo de
+                                    Cooperação 25/2018 firmado entre o Estado, Tribunal de Justiça e Cartórios de
+                                    Protestos, o sistema de protestos é
+                                    feito através de integração automática entre as instituições, isto é, por
+                                    sistema eletrônico.
+                                </Accordion.Body>
+
+                                <Accordion.Body>
+                                    <strong>2</strong> - Segundo a Lei de Protestos (Lei
+                                    Federal 9.492, de 1997), uma vez que o título foi remetido para o cartório de
+                                    protestos, o pagamento/parcelamento deverá ser feito exclusiva e diretamente no cartório que enviou a intimação (tanto do
+                                    crédito fiscal quanto das custas cartorárias). Não aceite que ninguém mais lhe
+                                    cobre por este serviço.
+                                </Accordion.Body>
+
+                                <Accordion.Body>
+                                    <strong>3</strong> - As cartas de anuência do apresentante 914 (Procuradoria-Geral do
+                                    Estado) são emitidas automaticamente após o parcelamento ou pagamento, no
+                                    prazo de três à cinco dias úteis. Este prazo se dá devido à necessidade de
+                                    compensação bancária.  Nesta situação,
+                                    não é preciso fazer este pedido, pois a carta de anuência chegará automaticamente
+                                    ao cartório de protestos e ficará armazenada na CENPROT (Central de Protestos).
+                                </Accordion.Body>
+
+                                <Accordion.Body>
+                                    <strong>4</strong> - As cartas de anuência para o apresentante 911 (Secretaria de Estado de
+                                    Fazenda), ou seja, protestos anteriores à maio de 2022, deverão utilizar
+                                    este requerimento.
+                                </Accordion.Body>
+
+                                <Accordion.Body>
+                                    <strong>5</strong> - Utilize este requerimento, também, caso tenha passado o prazo do item 3,
+                                    pois como toda plataforma que depende da internet e de integrações entre
+                                    sistemas, pode em alguns momentos apresentar instabilidades.
                                 </Accordion.Body>
                             </Accordion.Item>
                         </Accordion>
