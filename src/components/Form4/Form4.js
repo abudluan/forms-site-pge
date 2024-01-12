dc4cae1db7f7107ba6994191ae90cb4156bda5b7
leonjames-san <leonprata47@gmail.com>
2024-01-12 16:31:29 -0300

    chore: form1 copiado e adaptado para form4

--- src/components/Form4/Form4.js
+++ src/components/Form4/Form4.js
@@ -1,27 +1,22 @@
 import React, { useState, useRef } from 'react';
-import '../Style/FormStyles.css';
-import { Form, Card, Button, Container } from 'react-bootstrap';
+import { Button, Card, Container, Form } from 'react-bootstrap';
 import InputMask from 'react-input-mask';
+import '../Style/FormStyles.css';
 
 const Form4 = () => {
   const [submit, setSubmit] = useState(false);
   const [loading, setLoading] = useState(false);
-  const [formData, setFormData] = useState({
-   
-    'entry.1665899281': '',
-   
-  });
+  const [formData, setFormData] = useState({'entry.1665899281': '',});
   const [validated, setValidated] = useState(false);
 
   const handleInputData = (input) => (e) => {
-    const { type, checked, value } = e.target;
+    const { value } = e.target;
 
-    // Adicione uma verificação para o campo CPF/CNPJ
     const maskedValue = input === 'entry.1665899281' ? applyMask(value) : value;
 
     setFormData((prevState) => ({
       ...prevState,
-      [input]: type === 'checkbox' ? (checked ? 'Sim' : 'Não') : maskedValue,
+      [input]: maskedValue
     }));
   };
 
@@ -33,7 +28,8 @@ const Form4 = () => {
     } else {
       setSubmit(true);
       try {
-        const url = `https://docs.google.com/forms/d/e/1FAIpQLSfey3UPctkhqlQjoQM8z7ckO2VW9q4Lu0fU_CA_ZaJDLjI6bQ/viewform?usp=pp_url&entry.1665899281=${formData['entry.1665899281']}`;
+        const url = `https://docs.google.com/forms/d/e/1FAIpQLSfey3UPctkhqlQjoQM8z7ckO2VW9q4Lu0fU_CA_ZaJDLjI6bQ/viewform?usp=pp_url
+        &entry.1665899281=${formData['entry.1665899281']}`;
         const res = await fetch(url, {
           method: 'POST',
           headers: {
@@ -45,56 +41,17 @@ const Form4 = () => {
         }
       }
       catch (error) {
-        console.error('Erro ao fazer a solicitação:', error);
+        console.error('Ocorreu o seguinte erro ao fazer a solicitação:', error);
       }
       finally {
         setLoading(false);
+        
       }
     }
     setValidated(true);
   }
 
-  const fileInputRefs = {
-    fileInputRef1: useRef(null),
-  };
-
-  const handleFileUpload = async (fileInputRef, formDataKey) => {
-    setLoading(true);
-    const files = fileInputRef.current.files;
-    if (files.length > 0) {
-      const formData = new FormData();
-      for (let file of files) {
-        formData.append('files', file);
-      }
-      try {
-        const response = await fetch('https://fullstackers.com.br:7443/upload', {
-          method: 'POST',
-          body: formData,
-        });
-        if (response.ok) {
-          const data = await response.json();
-          setFormData((prevState) => ({
-            ...prevState,
-            [formDataKey]: data.files[0].id,
-          }));
-          console.log(`id = ${data.files[0].id}`);
-        }
-        else {
-          console.error('Falha ao fazer upload do arquivo');
-        }
-      }
-      catch (error) {
-        console.error('Erro ao enviar arquivo:', error);
-      }
-      finally {
-        setLoading(false);
-      }
-    }
-  };
-
-  const handleFileUpload1 = () => handleFileUpload(fileInputRefs.fileInputRef1, 'entry.1437382390');
-
-  const handleNewFormClick = () => {
+    const handleNewFormClick = () => {
     window.location.reload();
   }
 
@@ -126,30 +83,35 @@ const Form4 = () => {
             <h3 className='mt-2'><strong>Entrada do Contribuinte</strong></h3>
             
           </Card.Header>
-
           <Card.Body>
             <Form noValidate validated={validated} onSubmit={handleSubmit} target='_self'>
-                <Form.Group className='mb-3'>
+              <Form.Group className='mb-3'>
                 <Form.Label htmlFor='entry.1665899281' className='form-title'>CPF / CNPJ </Form.Label>
-                <Form.Text className='text-muted mx-2'>
+                <Form.Text className="text-muted mx-2">
                   (Somente números)
                 </Form.Text>
 
                 <Form.Control
-                  type='text'
-                  className='form-control'
-                  id='cpfCnpj'
+                  type="text"
+                  className="form-control"
+                  id="cpfCnpj"
                   onChange={(e) => handleInputData('entry.1665899281')(e)}
+                  onKeyPress={(event) => {
+                    if (/\D/.test(event.key)) {
+                      event.preventDefault();
+                    }
+                  }}
                   value={applyMask(formData['entry.1665899281'])}
-                  name='entry.1665899281'
+                  name="entry.1665899281"
                   maxLength={17}
+                  minLength={14}
                   required
                 />
                 <Form.Control.Feedback type="invalid">
                   Informe o CPF ou CNPJ
                 </Form.Control.Feedback>
               </Form.Group>
-              
+
               {loading && <div>Carregando...</div>}
               <Button className='btn-success px-5' type='submit' disabled={loading}>
                 Entrar
@@ -157,8 +119,7 @@ const Form4 = () => {
             </Form>
           </Card.Body>
         </Card >
-      )
-      }
+      )}
     </Container >
   );
 }
