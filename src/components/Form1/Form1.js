1fd7a4cb670d44d2bc3a0c532b66607c2f3fc920
leonjames-san <leonprata47@gmail.com>
2024-03-01 09:33:25 -0300

    chore: texto do form1 atualizado

--- src/components/Form1/Form1.js
+++ src/components/Form1/Form1.js
@@ -264,7 +264,7 @@ const Form1 = () => {
               </Form.Group>
 
               <Form.Group className='mb-3'>
-                <Form.Label htmlFor='entry.1259162928' className='form-title required'>CDA/CDAs (separe por vírgulas se mais de 1 CDA)</Form.Label>
+                <Form.Label htmlFor='entry.1259162928' className='form-title required'>CDA/CDAs (somente os números separados por vírgulas)</Form.Label>
                 <Form.Control
                   type='text'
                   className='form-control'
